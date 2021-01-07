587ec263b6c939cdfe8f29d81272ff16ba649af5
Jake Hiney <hiney.jake@outlook.com>
2021-01-07 10:50:57 -0600

    picker stylesheet

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap'
+import './componentStyle/picker.css'
 
 
 export default class Picker extends Component {
@@ -10,8 +11,8 @@ export default class Picker extends Component {
       <div>
         <Container>
           <Row>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
+            <Col id="left">1 of 2</Col>
+            <Col id="right">2 of 2</Col>
           </Row>
         </Container>
       </div>
