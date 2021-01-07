e389b3c1500d7393e8a6de6b7e5d7b206c41ab7f
Jake Hiney <hiney.jake@outlook.com>
2021-01-07 10:33:23 -0600

    add left and right side js files

---
+++ src/components/Left.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
+
+export default class Left extends Component {
+  static displayName = Left.name;
+
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Right.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
+
+export default class Right extends Component {
+  static displayName = Right.name;
+
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
