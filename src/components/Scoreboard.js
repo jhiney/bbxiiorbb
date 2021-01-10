511e3383c0edd81374d2faf43076f4d09d687d93
jhiney <56890661+jhiney@users.noreply.github.com>
2021-01-10 10:44:27 -0600

    Add scoreboard stylesheet

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
+import './componentStyle/Scoreboard.css'
 
 
 export default class Scoreboard extends Component {
