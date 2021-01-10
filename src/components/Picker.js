7077afe9ae20004ca0d06ece85034f87a1dd9475
jhiney <56890661+jhiney@users.noreply.github.com>
2021-01-10 10:39:59 -0600

    Update Picker.js
    changes to identify what is clicked

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap'
 import './componentStyle/picker.css'
 
-
+ const leftID = "left";
+const rightID = "right";
 export default class Picker extends Component {
   static displayName = Picker.name;
+
+ 
 
   render(){
     return(
       <div>
         <Container>
           <Row>
-            <Col onClick = {() => {this.alertTest()}} id="left">1 of 2</Col>
-            <Col id="right">2 of 2</Col>
+            <Col onClick = {() => {this.alertTest(leftID)}} id={leftID}>1 of 2</Col>
+            <Col onClick = {() => {this.alertTest(rightID)}} id={rightID}>2 of 2</Col>
           </Row>
         </Container>
       </div>
     );
   }
-  alertTest() {
-  alert("I am an alert box!");
+  alertTest(e) {
+  alert(e);
 }
 }
