a5f4db6f3b27d3009ee4e47c306b2433ad55d436
Jake Hiney <hiney.jake@outlook.com>
2021-01-12 13:14:18 -0600

    Update App.js to be in line with other components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home'
 
-// TODO: rewrite this to be in line with all the other components
-const App = () => {
-  return (
-    <div>
-    <Home />
-    </div>
-  );
-}
+export default class App extends Component {
+  static displayName = App.name;
 
-export default App;
+  render(){
+    return(
+      <div>
+      <Home />
+      </div>
+    )
+  }
+}
