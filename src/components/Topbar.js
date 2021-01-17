9af8c06e2c8a3501f05eb0d8d8a7c36d49aa6031
jhiney <56890661+jhiney@users.noreply.github.com>
2021-01-17 11:26:40 -0600

    Update Topbar.js
    uneeded search bar and change the brand title

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -11,14 +11,10 @@ export default class Topbar extends Component {
     return(
       <div>
       <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Big XII Football, or Basketball?</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
         </Navbar>
       </div>
     );
