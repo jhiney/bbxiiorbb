b67a057395a8b84d3c00b3000393c9f79369fa65
Jake Hiney <hiney.jake@outlook.com>
2021-01-21 13:16:18 -0600

    Test: Logged out

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home'
 
 export default class App extends Component {
   static displayName = App.name;
-//I am testing out Git Kraken
+//Lets see what happens now...
   render(){
     return(
       <div>
